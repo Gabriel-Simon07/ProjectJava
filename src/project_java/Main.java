cdf1753b0d536502a70cf8c9428f76006b2b2ee9
Gabriel Simon <gabrielsimon775@gmail.com>
2021-10-21 19:31:05 -0300

    Usando o printf para imprimir somente duas casas decimais

--- src/project_java/Main.java
+++ src/project_java/Main.java
@@ -3,8 +3,12 @@ package project_java;
 public class Main {
 
 	public static void main(String[] args) {
+		int idade = 25;
+		double altura = 1.895874;
 		String nome = "Gabriel";
-		System.out.println("Meu nome é : " + nome);
+		System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e tenho "
+				+ altura + " de altura.");
+		System.out.printf("%.2f%n", altura);
 
 	}
 
