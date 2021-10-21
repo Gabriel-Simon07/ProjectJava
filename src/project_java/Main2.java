62d53a85252726c7d8d5dc3b8811b1007b262af7
Gabriel Simon <gabrielsimon775@gmail.com>
2021-10-21 20:12:43 -0300

    Usando Locale e setando para US e também usando os marcadores de variáveis e interpolando os valores dentro do printf

---
+++ src/project_java/Main2.java
@@ -0,0 +1,21 @@
+package project_java;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		String procuct1 = "Computer";
+		String procuct2 = "Office Desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'f';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		
+		
+	}
+
+}
