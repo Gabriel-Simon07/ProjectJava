95a99e4da64d9c07859c30b20671c9094244c943
Gabriel Simon <gabrielsimon775@gmail.com>
2021-10-22 22:52:53 -0300

    Usando o método printf com expresão matemática

--- src/project_java/Main2.java
+++ src/project_java/Main2.java
@@ -1,5 +1,7 @@
 package project_java;
 
+import java.util.Locale;
+
 public class Main2 {
 
 	public static void main(String[] args) {
@@ -8,13 +10,20 @@ public class Main2 {
 		
 		int age = 30;
 		int code = 5290;
-		char gender = 'f';
+		char gender = 'F';
 		
 		double price1 = 2100.0;
 		double price2 = 650.50;
 		double measure = 53.234567;
 		
+		System.out.printf("Products:%n%s which price is %.2f%n%s"
+				+ "Record: %d years old, code %d and gender %s %nMeasue with "
+				+ "eight decimal places: %.8f%nRouded (three decimal places): "
+				+ "%.3f%n", 
+				procuct1, price1, procuct2, age, code, gender, measure, measure);
 		
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f", measure);
 		
 	}
 
---
+++ src/project_java/Main3.java
@@ -0,0 +1,17 @@
+package project_java;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		int x,y,z;
+		
+		x = 10;
+		y = 10;
+		z = 10;
+		
+		int valorCalculado = 10 + (x * y) / z;
+		
+		System.out.printf("O valor total calculado é %d", valorCalculado);
+	}
+
+}
