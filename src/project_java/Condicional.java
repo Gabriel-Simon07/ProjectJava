99e379a7dccfe8ce902c4c789abc979012b548e3
Gabriel Simon <gabrielsimon775@gmail.com>
2021-11-02 21:04:07 -0300

    Implementada a classe Condicional contendo condicionais verificando o valor informado

---
+++ src/project_java/Condicional.java
@@ -0,0 +1,19 @@
+package project_java;
+
+public class Condicional {
+
+	public static void main(String[] args) {
+		
+		short valor = 4;
+		
+		if(valor > 4)
+			System.out.println("Valor maior que 4");
+		
+		else if(valor == 4)
+			System.out.println("Valor igual a 4");
+		
+		else
+			System.out.println("Valor menor que 4");
+	}
+
+}
