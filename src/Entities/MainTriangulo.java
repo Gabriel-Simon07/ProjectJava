4d37db37536b5954c6f174287f3be07a0478b553
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-02 21:38:00 -0300

    Posto a função matemática para dentro de um método fica dentro da classe Triangulo

--- src/Entities/MainTriangulo.java
+++ src/Entities/MainTriangulo.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import java.awt.geom.Area;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -26,12 +27,10 @@ public class MainTriangulo {
 		y.a = scanner.nextDouble();
 		y.b = scanner.nextDouble();
 		y.c = scanner.nextDouble();
+		
+		double areaX = x.area();
 
-		p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaY = y.area();
 
 		System.out.printf("O valor do triângulo X %.4f%n", areaX);
 		System.out.printf("O valor do triângulo Y %.4f%n", areaY);
--- src/Entities/Triangulo.java
+++ src/Entities/Triangulo.java
@@ -5,4 +5,9 @@ public class Triangulo {
 	double a;
 	double b;
 	double c;
+	
+	public double area() {
+		double p = (this.a + this.b + this.c) / 2.0;
+		return Math.sqrt(p * (p - this.a)* (p - this.b)* (p - this.c));
+	}
 }
