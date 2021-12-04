f69a7d328b2a6eb93746dc875868c2ea87586172
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-03 23:41:47 -0300

    Implementado propriedades da classe Product e método e adicionado classe MainProduct responsável por chamar as implementações da classe Product

---
+++ src/Entities/MainProduct.java
@@ -0,0 +1,13 @@
+package Entities;
+
+import java.util.Scanner;
+
+public class MainProduct {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		scanner.close();
+	}
+
+}
--- src/Entities/Product.java
+++ src/Entities/Product.java
@@ -2,4 +2,14 @@ package Entities;
 
 public class Product {
 
+	String nome;
+	double preco;
+	int quantidade;
+
+	public double valorTotalEmEstoque() {
+		
+	}
+	
+	
+	
 }
