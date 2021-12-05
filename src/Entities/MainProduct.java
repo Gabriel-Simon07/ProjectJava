4b893c3e043025e3a21ec2a348d4752eb86808ac
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-04 22:36:03 -0300

    Implementados os métodos de adicionar, retirar e ter o valor total dos produtos da classe Product

--- src/Entities/MainProduct.java
+++ src/Entities/MainProduct.java
@@ -1,12 +1,28 @@
 package Entities;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MainProduct {
 
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
-		
+
+		Product product = new Product();
+
+		System.out.println("Adicione as informações do produto : ");
+		System.out.println("Nome : ");
+		product.nome = scanner.next();
+		System.out.println("Price : ");
+		product.preco = scanner.nextDouble();
+		System.out.println("Quantidade em estoque : ");
+		product.quantidade = scanner.nextInt();
+
+		System.out.println("Nome : " + product.nome + ", preço " + product.preco + ", quantidade em estoque "
+				+ product.quantidade);
+
 		scanner.close();
 	}
 
--- src/Entities/Product.java
+++ src/Entities/Product.java
@@ -7,9 +7,15 @@ public class Product {
 	int quantidade;
 
 	public double valorTotalEmEstoque() {
-		
+		return this.preco * this.quantidade;
 	}
-	
-	
-	
+
+	public void adicionarProdutos(int quantidade) {
+		this.quantidade += quantidade;
+	}
+
+	public void removerProdutos(int quantidade) {
+		this.quantidade -= quantidade;	
+	}
+
 }
