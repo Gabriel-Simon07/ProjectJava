1864d7f87a7f3193ec0637aee1cbbcccd851694a
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-09 20:35:29 -0300

    Implementada classe para o fazer cálculo de conversão de valor de dólar para reais

---
+++ src/Entities/CotacaoDolar.java
@@ -0,0 +1,17 @@
+package Entities;
+
+public class CotacaoDolar {
+
+	static double precoDolar;
+	static double qtdDolar;
+	static double iof = 6;
+
+	public static double valorDolarIof() {
+		return  (precoDolar * qtdDolar) * iof / 100;
+	}
+
+	public static double dolarConvertidoReais() {
+		return  (precoDolar * qtdDolar) + valorDolarIof();
+	}
+
+}
