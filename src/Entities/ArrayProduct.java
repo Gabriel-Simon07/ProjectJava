407631d165a9ab07449eed439b34433db79fecc7
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-15 22:01:28 -0300

    Implementada classe quem tem um array de produto e faz a média dos valores desses produtos

---
+++ src/Entities/ArrayProduct.java
@@ -0,0 +1,32 @@
+package Entities;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ArrayProduct {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+
+		int qtdProdutos = scanner.nextInt();
+		Product[] array = new Product[qtdProdutos];
+
+		for (int i = 0; i < array.length; i++) {
+			scanner.nextLine();
+			String nome = scanner.next();
+			double preco = scanner.nextDouble();
+			array[i] = new Product(nome, preco);
+		}
+
+		double soma = 0.0;
+		for (int i = 0; i < array.length; i++) {
+			soma += array[i].getPreco();
+		}
+		double media = soma / qtdProdutos;
+		System.out.println("A média dos valores dos produtos é de " + media);
+		scanner.close();
+	}
+
+}
