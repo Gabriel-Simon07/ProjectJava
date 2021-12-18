4fc233df4c6ea072f57dcc5eaedb2e5fc54ea153
Gabriel Simon <gabrielsimon775@gmail.com>
2021-12-17 22:12:02 -0300

    Implementada classe de Aluguel, a classe de Aluguelrecebe nome e email do estudante para conseguir ficar num quarto alugado

---
+++ src/Entities/AluguelEstudantesMain.java
@@ -0,0 +1,41 @@
+package Entities;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AluguelEstudantesMain {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+
+		Aluguel[] aluguel = new Aluguel[10];
+
+		System.out.println("Informe a quantidade de quartos alugados: ");
+		int qtdAlunos = scanner.nextInt();
+
+		for (int i = 1; i <= qtdAlunos; i++) {
+			System.out.println("Aluguel " + i + " : ");
+			System.out.println("Nome : ");
+			scanner.nextLine();
+			String nome = scanner.nextLine();
+			System.out.println("Email : ");
+			String email = scanner.nextLine();
+			System.out.println("Qual o número do quarto : ");
+			int numeroQuarto = scanner.nextInt();
+
+			aluguel[numeroQuarto] = new Aluguel(nome, email);
+		}
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("\nQuartos alugados :");
+			if (aluguel[i] != null) {
+				System.out.println("Quarto número " + i + " : " + aluguel[i]);
+			}
+		}
+
+		scanner.close();
+	}
+
+}
