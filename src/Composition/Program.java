085e1ef1221ebf69e7cb85e4f760d2e939e233c8
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-21 20:24:46 -0300

    Program class is being implemented

--- src/Composition/Program.java
+++ src/Composition/Program.java
@@ -1,9 +1,34 @@
 package Composition;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the department's name : ");
+		String departmentName = scanner.nextLine();
+		System.out.println("Enter work  data : ");
+		System.out.println("Name : ");
+		String workerName = scanner.nextLine();
+		System.out.println("Level : ");
+		String workerLevel = scanner.nextLine();
+		System.out.println("Base salary : ");
+		double baseSalary = scanner.nextDouble();
+		
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+		
+		
+		
+		
+		
+		
+		scanner.close();
 	}
 
 }
--- src/Composition/Worker.java
+++ src/Composition/Worker.java
@@ -81,4 +81,4 @@ public class Worker {
 		}
 		return sum;
 	}
-}
+}		
